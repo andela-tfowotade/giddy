02d9fd492b941f0a064dc0f387180b401292ce03
temifowotade <temitope.fowotade@andela.com>
2016-09-23 11:35:52 +0100

    feat(set up repo): Initializes repository with ORM connection to SQLite

---
+++ giddy.rb
@@ -0,0 +1,13 @@
+require "sqlite3"
+
+module Giddy
+  class Database
+    def self.connect
+      @db = SQLite3::Database.new("app.db");
+    end
+
+    def self.execute(*query)
+      connect.execute(*query)
+    end
+  end
+end
